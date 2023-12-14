a41be12d45ad18907bdaa842daa338f127e950ab
riad2008004 <arifulislamriad5@gmail.com>
2023-12-15 01:16:06 +0600

    checked

--- src/sonar.cpp
+++ src/sonar.cpp
@@ -48,13 +48,19 @@ float sonarSearchR()
 {
     digitalWrite(trigR, LOW);
     delayMicroseconds(2);
+
     // Sets the trigPin HIGH (ACTIVE) for 10 microseconds
+
     digitalWrite(trigR, HIGH);
     delayMicroseconds(10);
     digitalWrite(trigR, LOW);
+
     // Reads the echoPin, returns the sound wave travel time in microseconds
+
     duration = pulseIn(echoR, HIGH, waitTime);
+
     // Calculating the distance
+
     distance = duration * 0.034 / 2; // Speed of sound wave divided by 2 (go and back)
     // Displays the distance on the Serial Monitor
     // Serial.print(distance);
